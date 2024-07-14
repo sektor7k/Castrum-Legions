8d21643d69049fa0712fc5d2ff10c01f5d6b57c3
sektor7k <omeraydin2112@gmail.com>
2024-07-14 18:57:58 +0300

    mongoDb connection

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try {
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
+
+        connection.on('connected', () => {
+            console.log('Mongoose connected to database');
+        })
+        connection.on('error', (err) => {
+            console.log('Mongoose connection error: ', err);
+        })
+
+    } catch (error) {
+        console.log('Mongoose connect Eror');
+        console.log(error);
+    }
+}
