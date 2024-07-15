3e073048cc13c7d5816b51f33eb7e0a1fd84676d
sektor7k <omeraydin2112@gmail.com>
2024-07-15 23:35:38 +0300

    onlogin

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,25 +1,49 @@
 "use client"
 import Link from "next/link";
-import { useState } from "react";
-import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 
 export default function LoginPage() {
+
+    const router = useRouter()
 
     const [user, setUser] = useState({
         email: "",
         password: ""
     })
+    const [buttonDisabled, setButtonDisabled] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const onLogin = async () => {
-
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/login", user);
+            console.log("Login success", response.data.message);
+            toast.success("Login success");
+            router.push("/profile")
+        } catch (error: any) {
+            console.log("Login failed", error.message);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
     }
+
+    useEffect(() => {
+        if(user.email.length > 0 && user.password.length > 0) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+    },[user]);
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>
-                Login
+                {loading ? "Processing" : "Login"}
             </h1>
             <hr />
             <label htmlFor="email">email</label>
@@ -45,7 +69,7 @@ export default function LoginPage() {
                 onClick={onLogin}
                 className=" p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 "
             >
-                Login here
+                {buttonDisabled ? "No Login" : "Login here"}
             </button>
             <Link href={"/signup"}>Visit signup page</Link>
         </div>
