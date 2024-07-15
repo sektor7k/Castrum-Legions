d3858d6d8c600362b55a7b8b9df715559e1c40b1
sektor7k <omeraydin2112@gmail.com>
2024-07-15 21:03:51 +0300

    user model db

---
+++ src/models/userModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+    isVerfied: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpire: Date,
+    verifyToken: String,
+    verifyExpire: Date,
+
+});
+
+const User = mongoose.models.User || mongoose.model("Users", userSchema);
+
+export default User;
