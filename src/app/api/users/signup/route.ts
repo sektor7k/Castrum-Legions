7e6c2d79a3cbb0e0324b2fec0a35fa37785bdc60
sektor7k <omeraydin2112@gmail.com>
2024-07-15 21:04:06 +0300

    signup route

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,44 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypts from "bcryptjs";
+
+
+
+connect()
+
+export async function POST(request: NextRequest) {
+    try {
+        const reqBody = await request.json();
+        const { username, email, password } = reqBody;
+
+        console.log(reqBody);
+
+        const user = await User.findOne({ email })
+
+        if (user) {
+            return NextResponse.json({ message: "User already exists" }, { status: 400 })
+        }
+
+        //hash password
+        const salt = await bcrypts.genSalt(10);
+        const hashedPassword = await bcrypts.hash(password, salt);
+
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+        const saveUser = await newUser.save();
+        console.log("sasa",saveUser);
+
+        return NextResponse.json({
+            message: "User created successfully",
+            success: true,
+            saveUser
+        })
+
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
+    }
+}
