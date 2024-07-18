bef8fc6d8e0d9740efd8394bd8277ecd8511e3bd
sektor7k <omeraydin2112@gmail.com>
2024-07-18 05:31:35 +0300

    login modified

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,77 +1,142 @@
-"use client"
-import Link from "next/link";
+"use client";
+
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import {
+    IconBrandGithub,
+    IconBrandGoogle,
+    IconBrandOnlyfans,
+} from "@tabler/icons-react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
-import toast from "react-hot-toast";
-
+import Link from "next/link";
 
 export default function LoginPage() {
 
-    const router = useRouter()
+    const router = useRouter();
 
     const [user, setUser] = useState({
         email: "",
         password: ""
     })
-    const [buttonDisabled, setButtonDisabled] = useState(false);
-    const [loading, setLoading] = useState(false);
 
     const onLogin = async () => {
         try {
-            setLoading(true);
+
             const response = await axios.post("/api/users/login", user);
             console.log("Login success", response.data.message);
-            toast.success("Login success");
-            router.push("/profile")
+            router.push("/profile2")
         } catch (error: any) {
             console.log("Login failed", error.message);
-            toast.error(error.message);
-        } finally {
-            setLoading(false);
         }
     }
-
-    useEffect(() => {
-        if(user.email.length > 0 && user.password.length > 0) {
-            setButtonDisabled(false);
-        } else {
-            setButtonDisabled(true);
-        }
-    },[user]);
-
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>
-                {loading ? "Processing" : "Login"}
-            </h1>
-            <hr />
-            <label htmlFor="email">email</label>
-            <input
-                id="email"
-                type="text"
-                value={user.email}
-                onChange={(e) => setUser({ ...user, email: e.target.value })}
-                placeholder="email"
-                className=" p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-            />
+        <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black border border-gray-700 ">
+            <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+                LOGIN
+            </h2>
+            <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+                    Castrum Legionsa hoş geldiniz. Unutmayın biz bir aileyiz
+                </p>
 
-            <label htmlFor="password">password</label>
-            <input
-                id="password"
-                type="password"
-                value={user.password}
-                onChange={(e) => setUser({ ...user, password: e.target.value })}
-                placeholder="password"
-                className=" p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-            />
-            <button
-                onClick={onLogin}
-                className=" p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 "
-            >
-                {buttonDisabled ? "No Login" : "Login here"}
-            </button>
-            <Link href={"/signup"}>Visit signup page</Link>
+            <div className="my-8" >
+                <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+
+                </div>
+                <LabelInputContainer className="mb-4">
+                    <Label htmlFor="email">Email Address</Label>
+                    <Input
+                        id="email"
+                        placeholder="projectmayhem@fc.com"
+                        type="email"
+                        value={user.email}
+                        onChange={(e) => setUser({ ...user, email: e.target.value })}
+                    />
+                </LabelInputContainer>
+                <LabelInputContainer className="mb-4">
+                    <Label htmlFor="password">Password</Label>
+                    <Input
+                        id="password"
+                        placeholder="••••••••"
+                        type="password"
+                        value={user.password}
+                        onChange={(e) => setUser({ ...user, password: e.target.value })}
+                    />
+                </LabelInputContainer>
+
+
+                <button onClick={onLogin}
+                    className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+                    type="submit"
+                >
+                    Login &rarr;
+                    <BottomGradient />
+                </button>
+                <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+                    <Link href={"/signup"}>Sign up if you don't have an account</Link>
+                </p>
+
+                <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+
+                <div className="flex flex-col space-y-4">
+                    <button
+                        className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                        type="submit"
+                    >
+                        <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+                        <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                            GitHub
+                        </span>
+                        <BottomGradient />
+                    </button>
+                    <button
+                        className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                        type="submit"
+                    >
+                        <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+                        <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                            Google
+                        </span>
+                        <BottomGradient />
+                    </button>
+                    <button
+                        className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                        type="submit"
+                    >
+                        <IconBrandOnlyfans className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+                        <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                            OnlyFans
+                        </span>
+                        <BottomGradient />
+                    </button>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
+
+const BottomGradient = () => {
+    return (
+        <>
+            <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+            <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+        </>
+    );
+};
+
+const LabelInputContainer = ({
+    children,
+    className,
+}: {
+    children: React.ReactNode;
+    className?: string;
+}) => {
+    return (
+        <div className={cn("flex flex-col space-y-2 w-full", className)}>
+            {children}
+        </div>
+    );
+};
+
