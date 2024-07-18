dc2056b0341754647e1ff5c986e7eb2ac2c3ff66
sektor7k <omeraydin2112@gmail.com>
2024-07-18 03:03:37 +0300

    verifiyemail page modeified

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -12,7 +12,9 @@ export default function VerifyEmailPage() {
 
     const verifyUserEmail = async () => {
         try {
+            console.log("buraya")
             await axios.post('/api/users/verifyemail', { token })
+            console.log("geldi")
             setVerified(true);
         } catch (error: any) {
             setError(true);
@@ -20,7 +22,45 @@ export default function VerifyEmailPage() {
         }
     }
 
+    useEffect(() => {
+        const urlToken = window.location.search.split("=")[1];
+        setToken(urlToken || "");
+    }, [])
+
+    useEffect(() => {
+        if (token.length > 0) {
+            verifyUserEmail();
+        }
+    }, [token])
+
     return (
-        <div> o ye</div>
+        <div className=" flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="text-4xl">
+                Verify Email
+            </h1>
+            <h2 className="p-2 bg-orange-500 text-black">
+                {token ? `${token}` : "no token"}
+            </h2>
+
+            {verified && (
+                <div>
+                    <h2 className="p-2 bg-green-500 text-black">
+                        Email verified
+                    </h2>
+                    <Link className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" href="/login">
+                        Login
+                    </Link>
+                </div>
+            )}
+
+            {error && (
+                <div>
+                    <h2 className="p-2 bg-red-500 text-black">
+                        Error
+                    </h2>
+                    
+                </div>
+            )}
+        </div>
     )
 }
