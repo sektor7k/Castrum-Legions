049975582ee4144cf4787c804667f81a485b7f03
sektor7k <omeraydin2112@gmail.com>
2024-07-18 05:53:03 +0300

    .

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,9 +1,13 @@
-export default function UserProfile({params}:any) {
+function UserProfile({params}:any) {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
-            {/* <p className="text-4xl">Profile page <span className="p-2 rounded bg-orange-500 text-black">{params.id}</span></p> */}
+            <p className="text-4xl">Profile page <span className="p-2 rounded bg-orange-500 text-black">{params.id}</span></p>
         </div>
     )
 }
+
+UserProfile.displayName = 'UserProfile';
+
+export default UserProfile;
